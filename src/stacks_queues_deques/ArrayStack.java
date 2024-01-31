52926c34836df3ef55a7a0ae3b5038e2a5af6cb4
Marat Ayaibergenov <marataya@gmail.com>
2024-01-31 16:37:45 +0600

    add: stack if and ArrayStack impl

---
+++ src/stacks_queues_deques/ArrayStack.java
@@ -0,0 +1,52 @@
+package stacks_queues_deques;
+
+import java.util.IllegalFormatException;
+import java.util.List;
+
+public class ArrayStack<E> implements Stack<E> {
+    private static final int CAPACITY = 1000;
+
+    private E[] data;
+    private int t = -1; // idx of a top element in stack
+
+    public ArrayStack() {
+        this(CAPACITY);
+    }
+    public ArrayStack(int capacity) {
+        data = (E[]) new Object[capacity];
+    }
+
+
+    @Override
+    public int size() {
+        return t + 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return t == -1;
+    }
+
+    @Override
+    public void push(E e) {
+        if (size() == data.length) throw new IllegalStateException("Stack is full");
+        data[++t] = e;  //increment before storing new item
+    }
+
+    @Override
+    public E top() {
+        if (isEmpty()) return null;
+        return data[t];
+    }
+
+    @Override
+    public E pop() {
+        if (isEmpty()) return null;
+        E answer = data[t];
+        data[t] = null; // dereference to help garbage collector
+        t--;
+        return answer;
+    }
+
+
+}
---
+++ src/stacks_queues_deques/Stack.java
@@ -0,0 +1,13 @@
+package stacks_queues_deques;
+
+public interface Stack<E> {
+    int size();
+
+    boolean isEmpty();
+
+    void push(E e);
+
+    E top();
+
+    E pop();
+}
