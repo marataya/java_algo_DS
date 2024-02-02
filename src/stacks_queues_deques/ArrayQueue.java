83af5f7c0b23fedff60139ab6717e59c0282a497
Marat Ayaibergenov <marataya@gmail.com>
2024-02-03 03:28:33 +0600

    add: Queue interface and ArrayQueue

---
+++ src/stacks_queues_deques/ArrayQueue.java
@@ -0,0 +1,58 @@
+package stacks_queues_deques;
+
+import java.util.Arrays;
+
+public class ArrayQueue<E> implements Queue<E> {
+    private static final int CAPACITY = 20;
+    private E[] data;
+    private int f;  // index of the front element
+    private int size = 0;
+
+    public ArrayQueue() {
+        this(CAPACITY);
+    }
+    public ArrayQueue(int capacity) {
+        data = (E[]) new Object[capacity]; // safe cast; compiler may give warning
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void enqueue(E e) {
+        if (size == data.length) throw new IllegalStateException("The queue is full");
+        int avail = (f + size) % data.length;
+        data[avail] = e;
+        size++;
+    }
+
+    @Override
+    public E dequeue() {
+        return null;
+    }
+
+    @Override
+    public E first() {
+        return null;
+    }
+
+    @Override
+    public E last() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        var aq = new ArrayQueue<Integer>();
+        aq.enqueue(5);
+        aq.enqueue(7);
+        aq.enqueue(9);
+        System.out.println(Arrays.toString(aq.data));
+    }
+}
---
+++ src/stacks_queues_deques/Queue.java
@@ -0,0 +1,13 @@
+package stacks_queues_deques;
+
+public interface Queue<E> {
+    int size();
+    boolean isEmpty();
+
+    void enqueue(E e);
+
+    E dequeue();
+
+    E first();
+    E last();
+}
